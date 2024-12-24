d82a156f47f6cc2285e0280fb46937e06fe52e52
Dmitry Klemeshov <d.klemeshov@inmar.team>
2024-12-24 19:34:53 +0500

    add isr

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,8 @@ const ArticleCard = ({ title, description, text, createAt, updateAt }: Article) 
   );
 };
 
+export const revalidate = 60;
+
 export default async function Home() {
   const data = await prisma.article.findMany();
 
